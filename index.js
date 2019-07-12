dcb25ee6453d26c638cfb1a9c40a8f9d36c03418
Villem Alango <villem.alango@gmail.com>
2019-07-12 17:31:10 +0300

    code cleaned

--- index.js
+++ index.js
@@ -15,16 +15,16 @@ let strict = assert.strict
 const E_RO = 'Property is read-only'
 
 const AssertionError = assert.AssertionError
-//  Some stuff works differently before v10.
-const nodeMajor      = 1 * process.version.substring(1).split('.')[0]
+//  Some stuff worked differently before v10.
+const nodeMajor = 1 * process.version.substring(1).split('.')[0]
 
 //  Global event that will be emitted every time when assertion throws.
 //  Parameters: errorInstance, cancelFunction.
 let eventType
 
-const inProduction = process.env.NODE_ENV === 'production'
+const notProduction = process.env.NODE_ENV !== 'production'
 
-if (!inProduction) {
+if (notProduction) {
   eventType = process.env.NODE_ASSERTION_EVENT === undefined
     ? 'BeforeAssertionIsThrown' : process.env.NODE_ASSERTION_EVENT
 
@@ -33,6 +33,7 @@ if (!inProduction) {
       try {
         return fn.apply(obj, args)
       } catch (error) {
+        //  In earlier Node.js versions, assert.throws threw just Error instance.
         if (eventType && (nodeMajor < 10 || error instanceof AssertionError)) {
           let isCanceled = false
           process.emit(assert.eventType, error, () => (isCanceled = true))
@@ -50,6 +51,7 @@ if (!inProduction) {
       let v = obj[key]
 
       if (v instanceof Function && key !== 'strict' && key !== 'AssertionError') {
+        //  Gotcha: assert === assert.ok
         v = key === 'ok' ? wrappedFn : wrap(obj, v)
       }
       wrappedFn[key] = v
@@ -63,21 +65,20 @@ if (!inProduction) {
 
 const addStaticsToAPI_ = (api) => {
   const dscr = Object.create(null)    //  Shield against prototype pollution.
-  const ro   = () => ((strict || assert).fail(E_RO))
+  const ro = () => ((strict || assert).fail(E_RO))
 
   dscr.enumerable = true
-  dscr.get        = () => nodeMajor
-  dscr.set        = ro
+  dscr.get = () => nodeMajor
+  dscr.set = ro
   Object.defineProperty(api, 'nodeMajor', dscr)
 
   dscr.get = () => eventType
-  if (!inProduction) dscr.set = (v) => (eventType = v)
+  if (notProduction) dscr.set = (v) => (eventType = v)
   Object.defineProperty(api, 'eventType', dscr)
 }
 
 addStaticsToAPI_(exports = module.exports = assert)
 
-if (strict) {
-  addStaticsToAPI_(strict)
-}
+if (strict) addStaticsToAPI_(strict)
+
 exports.strict = strict || assert   //  Upwards compatibility.
